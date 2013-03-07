7819d9f10750fb3aa28ea91d89b0e243cf987b77
Jeremy Fox <atljeremy@me.com>
2013-03-06 22:47:07 -0500

    Committing final project updates for submission

--- ProjectI/src/com/example/ProjectI/MainActivity.java
+++ ProjectI/src/com/example/ProjectI/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.ProjectI;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
+
+import java.util.Random;
 
 public class MainActivity extends Activity {
 
@@ -26,17 +27,19 @@ public class MainActivity extends Activity {
      *
      * The application must also utilize, at minimum, 1 of each of the following Android components:
      *
-     * Android resource value
+     * Android resource value √
      * TextView √
      * EditText √
      * Button √
-     * LinearLayout
+     * LinearLayout √
      */
 
     /**
-     * Global motorcycle array used by to hold the String values of all 5 motorcycles tat will be used
+     * Global motorcycle array used by to hold the String values of all 5 motorcycles that will be used
      */
     private static final String[] mMotorcycles = new String[5];
+    private EditText editText;
+    private TextView results;
 
     /**
      * Called when the activity is first created.
@@ -54,54 +57,115 @@ public class MainActivity extends Activity {
      * Used to create all custom UI Elements with one method call
      */
     private void createUIElements() {
-        createTextview();
-        createEditText();
-        createButton();
+        LinearLayout layout = createLinearLayout();
+
+        TextView howToPlay = createTextview(layout);
+        howToPlay.setText(getString(R.string.howToPlay));
+
+        createEditText(layout);
+        createButton(layout);
+
+        this.results = createTextview(layout);
     }
 
     /**
-     *
-     * @return a TextView which will be used in our custom UI
+     * Custom LinearLayout
      */
-    private TextView createTextview() {
-        TextView textView = new TextView(this);
+    private LinearLayout createLinearLayout() {
+        LinearLayout layout = new LinearLayout(this);
+        layout.setBackgroundColor(Color.BLACK);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setGravity(Gravity.CENTER_HORIZONTAL);
 
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(layoutParams);
-        this.addContentView(textView, params);
+        this.addContentView(layout, params);
+
+        return layout;
+    }
+
+    /**
+     * Custom TextView
+     */
+    private TextView createTextview(LinearLayout layout) {
+        TextView textView = new TextView(this);
+        layout.addView(textView, getLayoutParams());
         return textView;
     }
 
     /**
-     *
-     * @return an EditText which will be used in our custom UI
+     * Custom EditText
      */
-    private EditText createEditText() {
-        EditText editText = new EditText(this);
-
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(layoutParams);
-        this.addContentView(editText, params);
-        return editText;
+    private void createEditText(LinearLayout layout) {
+        this.editText = new EditText(this);
+        layout.addView(this.editText, getLayoutParams());
     }
 
     /**
-     *
-     * @return a Button which will be used in our custom UI
+     * Custom Button
      */
-    private Button createButton() {
+    private void createButton(LinearLayout layout) {
         Button button = new Button(this);
+        button.setText("Get Bike!");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "BOOM", Toast.LENGTH_LONG).show();
+
+                String toastMessage;
+                String resultsText;
+
+                if (MainActivity.this.editText.length() > 0) {
+                    int chosenIndex = -1;
+                    boolean isNumeric;
+
+                    try {
+                       chosenIndex = Integer.parseInt(MainActivity.this.editText.getText().toString()) - 1;
+                       isNumeric = true;
+                    } catch (Exception exception) {
+                       isNumeric = false;
+                    }
+
+                    if (!isNumeric || !motorCylceExistsAtIndex(chosenIndex)) {
+                        toastMessage = getString(R.string.pleaseChooseBetween);
+                        resultsText = getString(R.string.emptyString);
+                    } else {
+                        Random random = new Random();
+                        int min = 0, max = 4;
+                        int randomIndex = random.nextInt(max - min + 1) + min;
+
+                        if (randomIndex == chosenIndex) {
+                            toastMessage = getString(R.string.youWin);
+                        } else {
+                            toastMessage = getString(R.string.tryAgain);
+                        }
+
+                        StringBuilder finalResult = new StringBuilder(getString(R.string.results));
+                        finalResult.append(getString(R.string.youChose));
+                        finalResult.append(" " + motorcycleNameAtIndex(chosenIndex));
+                        finalResult.append(getString(R.string.iChose));
+                        finalResult.append(" " + motorcycleNameAtIndex(randomIndex));
+                        resultsText = finalResult.toString();
+                    }
+                } else {
+                    toastMessage = getString(R.string.emptyTextView);
+                    resultsText = getString(R.string.emptyString);
+                }
+
+                MainActivity.this.results.setText(resultsText);
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
             }
         });
 
+        layout.addView(button, getLayoutParams());
+    }
+
+    /**
+     * Used in the custom view creation methods to set the layout params
+     * @return LayoutParams
+     */
+    private ViewGroup.LayoutParams getLayoutParams() {
         ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(layoutParams);
-        this.addContentView(button, params);
-        return button;
+        return new ViewGroup.LayoutParams(layoutParams);
     }
 
     /**
@@ -112,7 +176,7 @@ public class MainActivity extends Activity {
      */
     private String motorcycleNameAtIndex(int index) {
         String motorcycleName = "";
-        if (doesMotorCylceExistAtIndex(index))
+        if (motorCylceExistsAtIndex(index))
             motorcycleName = this.mMotorcycles[index];
         return motorcycleName;
     }
@@ -121,15 +185,15 @@ public class MainActivity extends Activity {
      * Populates the mMotorcycles array with the 5 bikes we'll be using
      */
     private void populateMotorcycleArray() {
-        String[] motorcyleNames = new String[this.mMotorcycles.length];
-        motorcyleNames[0] = "Ducati Monster";
-        motorcyleNames[1] = "Honda CBR";
-        motorcyleNames[2] = "Suzuki GXSR";
-        motorcyleNames[3] = "Kawasaki Ninja";
-        motorcyleNames[4] = "Harley-Davidson V-Rod";
+        String[] motorcycleNames = new String[this.mMotorcycles.length];
+        motorcycleNames[0] = getString(R.string.ducati);
+        motorcycleNames[1] = getString(R.string.honda);
+        motorcycleNames[2] = getString(R.string.suzuki);
+        motorcycleNames[3] = getString(R.string.kawasaki);
+        motorcycleNames[4] = getString(R.string.harleyDavidson);
 
         for (int i=0; i<this.mMotorcycles.length; i++) {
-            this.mMotorcycles[i] = motorcyleNames[i];
+            this.mMotorcycles[i] = motorcycleNames[i];
         }
     }
 
@@ -139,7 +203,7 @@ public class MainActivity extends Activity {
      * @param index
      * @return a boolean signifying whether or not a motorcycle exists at the given index
      */
-    private boolean doesMotorCylceExistAtIndex(int index) {
+    private boolean motorCylceExistsAtIndex(int index) {
         boolean motorcycleExists = false;
         if (this.mMotorcycles.length >= index) {
             String motorcycleFromArray = this.mMotorcycles[index];
